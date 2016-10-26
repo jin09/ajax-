a2e794c9acae9c6df0894d221a8e23f73c3fa0ac
jin09 <supergautam995@gmail.com>
2016-10-26 22:22:48 +0530

    NYTIMES API AJAX used to fetch articles from server

--- js/script.js
+++ js/script.js
@@ -21,8 +21,25 @@ function loadData() {
     var streetviewURL = "http://maps.googleapis.com/maps/api/streetview?size=600x400&location="
     						+ addr + "";
     $body.append("<img class='bgimg' src='"+streetviewURL+"'>");
- 
-    // YOUR CODE GOES HERE!
+
+    //NYTIMES AJAX request
+
+    var nytimesURL = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q="
+    + cityString + "&sort=newest&api-key=5ef42faf14294c03981fdab4e83e602f";
+    var articles;
+    $.getJSON(nytimesURL, function(data){
+    	$nytHeaderElem.text("NEW YORK TIMES ARTICLES ABOUT "+ cityString);
+    	console.log(data);
+    	articles = data.response.docs;
+    	for(var i=0;i<articles.length;i++){
+    		var article = articles[i];
+    		$nytElem.append("<li class='article'>"+
+    			'<a href="'+article.web_url+'">'+article.headline.main+
+    			'</a>'+
+    			'<p>' + article.snippet+'</p>'
+    			+'</li>');
+    	}
+    });
 
     return false;
 };
